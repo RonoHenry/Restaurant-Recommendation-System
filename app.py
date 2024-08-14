01fa39f42a8b4342e8d5abc1b81d89fe0a5291bb
AtomHarris <harrislukundi@yahoo.com>
2024-08-14 20:13:13 +0300

    adding different states filter feature

--- app.py
+++ app.py
@@ -5,11 +5,18 @@ from streamlit_option_menu import option_menu
 from deployment.home import render_home_page
 
 
-
+st.set_page_config(
+    page_title="Gourmet Gurus App",
+    page_icon="	:gear:",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://github.com/AtomHarris/Waste-Image-Classification',
+        'Report a bug': 'https://github.com/AtomHarris/Waste-Image-Classification',
+        'About': "# Here to help with all your waste issues!"
+    }
+)
 # Company Logo
 st.sidebar.image('Images/image5.png', use_column_width=True)
-
-
 
 # Home Page Option Menu Initialization
 selected = option_menu(

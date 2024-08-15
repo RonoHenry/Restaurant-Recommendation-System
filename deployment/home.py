7b6abc31112a5d3bde7eb5a587b43fe8eedd3ebc
AtomHarris <harrislukundi@yahoo.com>
2024-08-15 09:58:22 +0300

    resizing folium map

--- deployment/home.py
+++ deployment/home.py
@@ -132,7 +132,7 @@ def render_home_page():
                     st.subheader(f"{info['name']} Information", divider=True)
                     col1, col2 = st.columns([2,1])
                     with col2:
-                        with st.container(height=520,border=True):
+                        with st.container(height=420,border=True):
                             # st.write(f"**Restaurant Name:** {info['name']}")
                             st.write(f"**State:** {info['state']}")
                             st.write(f"**City:** {info['city']}")
@@ -146,7 +146,7 @@ def render_home_page():
                             
                                                 
                     with col1:
-                        with st.container(height=520, border=True):    
+                        with st.container(height=420, border=True):    
                             if 'latitude' in info and 'longitude' in info:
                                 latitude = info['latitude']
                                 longitude = info['longitude']
@@ -166,7 +166,7 @@ def render_home_page():
                                     ).add_to(m)
 
                                     
-                                    st_folium(m, width=700, height=400)
+                                    st_folium(m, width=700, height=300)
 
                                     route = f"http://maps.google.com/maps?z=12&t=m&q=loc:{latitude}+{longitude}"
                                     st.button(f"[Get Directions]({route})")
